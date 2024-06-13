5d18ddbaf4cc6ee11ea41b4ffd35a159906d969d
hykal1 <hykalhakim312@gmail.com>
2024-06-14 03:10:15 +0700

    update profile screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation ("androidx.compose.ui:ui-tooling-preview:1.6.7")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("androidx.activity:activity-compose:1.9.0")
+    implementation ("androidx.compose.material:material-icons-extended:1.5.1")
 
     // ViewModel and LiveData
     implementation (libs.androidx.lifecycle.viewmodel.compose)
