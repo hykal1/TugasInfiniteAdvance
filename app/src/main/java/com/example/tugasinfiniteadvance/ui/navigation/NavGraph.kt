9702464a8689064c8d2de5aca222df060211ca98
hykal1 <hykalhakim312@gmail.com>
2024-06-14 02:43:43 +0700

    add registration screen and welcome screen

--- app/src/main/java/com/example/tugasinfiniteadvance/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/tugasinfiniteadvance/ui/navigation/NavGraph.kt
@@ -7,12 +7,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tugasinfiniteadvance.ui.screens.home.SholatNowScreen
-import com.example.tugasinfiniteadvance.ui.screens.profile.Profile
+import com.example.tugasinfiniteadvance.ui.screens.profile.UserProfileScreen
 
 @Composable
 fun AppNavGraph(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
     NavHost(navController = navController, startDestination = "SholatNow") {
         composable("SholatNow") { SholatNowScreen() }
-        composable("Profile") { Profile() }
+        composable("Profile") { UserProfileScreen() }
     }
 }
